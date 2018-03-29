b6931e9f0534b722ebc5118b2d5419492493cd95
unknown <rogerio.ferreria@blackcirrus.com.br>
2018-03-28 21:57:16 -0300

    Correção de Products 8

--- frontend/src/app/dashboard/shared/pipe/sort.pipe.ts
+++ frontend/src/app/dashboard/shared/pipe/sort.pipe.ts
@@ -4,29 +4,28 @@ import {Pipe, PipeTransform} from "@angular/core";
 export class SortByPipe implements PipeTransform {
 
     transform(array:Array<any>, args:string, args2:string):Array<string> {
-        if (array == null) {
-            return array;
+        if (array !== undefined) {
+            array.sort((a:any, b:any) => {
+                if (args2) {
+                    if (a[args][args2] < b[args][args2]) {
+                        return -1;
+                    } else if (a[args][args2] > b[args][args2]) {
+                        return 1;
+                    } else {
+                        return 0;
+                    }
+                } else {
+                    if (a[args] < b[args]) {
+                        return -1;
+                    } else if (a[args] > b[args]) {
+                        return 1;
+                    } else {
+                        return 0;
+                    }
+                }
+            });
         }
-
-        array.sort((a:any, b:any) => {
-            if (args2) {
-                if (a[args][args2] < b[args][args2]) {
-                    return -1;
-                } else if (a[args][args2] > b[args][args2]) {
-                    return 1;
-                } else {
-                    return 0;
-                }
-            } else {
-                if (a[args] < b[args]) {
-                    return -1;
-                } else if (a[args] > b[args]) {
-                    return 1;
-                } else {
-                    return 0;
-                }
-            }
-        });
+       
         return array;
     }
 }
